5201a130f9b1c1e795cef8e4d58786494f603e84
seanieStack <seaniestack345@gmail.com>
2024-02-19 12:21:50 +0000

    setup basic server stuff, using port 6558

--- CS4076P1Server/src/main/java/seanie/mark/cs4076p1server/CS4076P1Server.java
+++ CS4076P1Server/src/main/java/seanie/mark/cs4076p1server/CS4076P1Server.java
@@ -4,13 +4,42 @@
 
 package seanie.mark.cs4076p1server;
 
+import java.io.IOException;
+import java.net.*;
+
 /**
  *
  * @author Seani
  */
 public class CS4076P1Server {
 
+    private static ServerSocket servSock;
+    private static final int PORT = 6558;
+    private static int clientConnections = 0;
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try{
+            servSock = new ServerSocket(PORT);
+        } catch(IOException e){
+            System.out.println("Unable to attrach port!");
+            System.exit(1);
+        }
+        
+        do{
+            run();
+        }while(true);
     }
+    private static void run(){
+        
+        Socket link = null;
+        
+        try{
+            link = servSock.accept();
+            clientConnections++;
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
+
