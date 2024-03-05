350be20aea128241d762d770d53b495076f8d089
CallanMark <144827341+CallanMark@users.noreply.github.com>
2024-03-05 00:00:03 +0000

    Add getTimes and getDays methods

--- CS4076P1Client-nogui-test/src/main/java/seanie/mark/cs4076p1client/nogui/test/Utillity.java
+++ CS4076P1Client-nogui-test/src/main/java/seanie/mark/cs4076p1client/nogui/test/Utillity.java
@@ -30,5 +30,14 @@ public class Utillity {
         });
     }
 
+     public static String[] getTimes() {
+        return new String[]{"09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"};
+
+    }
+
+    public static String[] getDays() {
+        return new String[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"} ;
+    }
+
 
 }
