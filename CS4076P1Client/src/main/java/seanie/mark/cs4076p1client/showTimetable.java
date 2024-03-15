70db5be45b2e1e0ee3792cbdf9f2464f3b28c631
CallanMark <144827341+CallanMark@users.noreply.github.com>
2024-03-15 23:16:44 +0000

    Add new features , connection of 'te' with server

--- CS4076P1Client/src/main/java/seanie/mark/cs4076p1client/showTimetable.java
+++ CS4076P1Client/src/main/java/seanie/mark/cs4076p1client/showTimetable.java
@@ -12,6 +12,7 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.DialogPane;
 
 
 
@@ -40,7 +41,7 @@ public class showTimetable {
         Scene scene = new Scene(layout, 600, 600); // Create the scene with the layout
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm()); //Added to fix Mac font issue 
         stage.setScene(scene); // Set the scene on the stage
-        stage.setTitle("Welcome Screen"); // Optional: set a title for the window
+        stage.setTitle("Show timetable"); 
 
 
         Label moduleLabel = new Label("Module:");
@@ -48,12 +49,44 @@ public class showTimetable {
 
         HBox root2 = new HBox();
         root2.setSpacing(10);
-        Button fullTimetable = new Button("Display full timetable "); //TODO: Fix UI so its better , review with seanie
-        Button moduleTimetable = new Button("Display module timetable");
+        
         Button submitButton = new Button("Submit");
         Button backButton = new Button("Return");
-        root2.getChildren().addAll(submitButton, backButton,fullTimetable,moduleTimetable);
+        root2.getChildren().addAll(submitButton, backButton);
         backButton.setOnAction(e -> returnToMain.run());
+
+        GridPane timetable = new GridPane();
+         
+        // Creating the timetable 
+        
+        String [] times = Utillity.getTimes();
+        String [] days = Utillity.getDays();
+
+        timetable.setPadding(new javafx.geometry.Insets(15,15,15,15)); //Padding , Vgap and Hgap all need resizing
+        timetable.setVgap(15);
+        timetable.setHgap(15);
+
+
+        Label mondayLabel = new Label("Monday");
+        Label tuesdayLabel = new Label("Tuesday");
+        Label wednesdayLabel = new Label("Wednesday");
+        Label thursdayLabel = new Label("Thursday");
+        Label fridayLabel   = new Label("Friday");
+
+        GridPane.setConstraints(mondayLabel,1,0);
+        GridPane.setConstraints(tuesdayLabel,2,0);
+        GridPane.setConstraints(wednesdayLabel,3,0);
+        GridPane.setConstraints(thursdayLabel,4,0);
+        GridPane.setConstraints(fridayLabel,5,0);
+        
+
+        for (int j = 0 ; j < times.length; j++){
+            Label timeLabel = new Label(times[j]);
+            GridPane.setConstraints(timeLabel,0,j+1);
+            timetable.getChildren().add(timeLabel);
+        }
+       
+        timetable.getChildren().addAll(mondayLabel,tuesdayLabel,wednesdayLabel,thursdayLabel,fridayLabel);
 
         Text displayText = new Text();
 
@@ -74,6 +107,9 @@ public class showTimetable {
                                 notFoundAlert.setTitle("Error : Module not found");
                                 notFoundAlert.setHeaderText(null);
                                 notFoundAlert.setContentText("This module does not exist in our record , try adding it by selecting 'return' ,'add module'");
+                                DialogPane dialogPane = notFoundAlert.getDialogPane();
+                                dialogPane.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+                                dialogPane.getStyleClass().add("my-dialog");
                                 notFoundAlert.show();
                                 break;
                         }
@@ -81,55 +117,31 @@ public class showTimetable {
                     } catch (Exception ex) {
                         displayText.setText("Error Sending Display Schedule Request");
                     }
+
+                     try {
+                        out.println("te " + userModule);
+                        String response = in.readLine();
+                        System.out.println(response);
+                        if (!response.contains(userModule)) {
+                            displayText.setText("Error occured : Module not found");
+                        } else if (!VerifyInput.isValidModule(userInputModule.getText())) {
+                            displayText.setText(userModule + ": is not a valid module");
+                            Alert nonValidAlert = new Alert(Alert.AlertType.ERROR);
+                            nonValidAlert.setTitle("Submission error : Invalid Module");
+                            nonValidAlert.setContentText("Module : " + userModule + " does not follow the required module code formatting e.g CS4076");
+                            nonValidAlert.show();
+                        }else {
+                            int nodes[] =  Utillity.moduleNodes(response);
+                            String parts []= Utillity.splitPayload(response);
+                            Label responseLabel = new Label(parts[0]+ " " +parts[3]);
+                           // GridPane.setConstraints(responseLabel,nodes[0],nodes[1] );
+                            GridPane.setConstraints(responseLabel,nodes[0],nodes[1] );
+                            timetable.getChildren().add(responseLabel);
+                        }
+                    } catch (Exception ex) {
+                        out.println("Error displaying time table");
+                    }
                 });
-
-
-//        Utillity.qForTermination(scene); //Q to terminate
-
-        GridPane timetable = new GridPane();
-
-        String [] times = Utillity.getTimes();
-        String [] days = Utillity.getDays();
-
-        timetable.setPadding(new javafx.geometry.Insets(100,100,100,100)); //Padding , Vgap and Hgap all need resizing
-        timetable.setVgap(100);
-        timetable.setHgap(100);
-
-
-        Label mondayLabel = new Label("Monday");
-        Label tuesdayLabel = new Label("Tuesday");
-        Label wednesdayLabel = new Label("Wednesday");
-        Label thursdayLabel = new Label("Thursday");
-        Label fridayLabel   = new Label("Friday");
-
-        GridPane.setConstraints(mondayLabel,1,0);
-        GridPane.setConstraints(tuesdayLabel,2,0);
-        GridPane.setConstraints(wednesdayLabel,3,0);
-        GridPane.setConstraints(thursdayLabel,4,0);
-        GridPane.setConstraints(fridayLabel,5,0);
-
-
-        /**
-        for (int i =1 ; i < days.length+1; i++) {  // Populate days
-            Label dayLabel = new Label(days[i]);
-            GridPane.setConstraints(dayLabel,i,0);
-            timetable.getChildren().add(dayLabel);
-        }
-        **/ //This could work but doesn't right now
-
-        for (int j = 0 ; j < times.length; j++){
-            Label timeLabel = new Label(times[j]);
-            GridPane.setConstraints(timeLabel,0,j+1);
-            timetable.getChildren().add(timeLabel);
-        }
-
-        //Example usage
-        String payload = "<CS4096><09:00-10:00><Monday><KGB-12>";
-        int nodes[] =  Utillity.moduleNodes(payload);
-        String parts []= Utillity.splitPayload(payload);
-        Label exampleLabel = new Label(parts[0]+ " " +parts[1]);
-        GridPane.setConstraints(exampleLabel,nodes[0],nodes[1] );
-        timetable.getChildren().addAll(mondayLabel,tuesdayLabel,wednesdayLabel,thursdayLabel,fridayLabel,exampleLabel);
 
 
         stage.setOnCloseRequest((WindowEvent we) -> Utillity.quitApp(in, out));
@@ -137,14 +149,7 @@ public class showTimetable {
         // Adding components to the GridPane
         layout.getChildren().addAll(moduleLabel, userInputModule, root2, displayText,timetable);
 
-        /**
-         * Some comments on commit:
-         * Added the axis's with days and time , needs resizing its far too big at present
-         * Added example usage , This will need to be changed to be more efficient and include getting data from DB
-         * I am aware that the payload string in example usage takes the wrong format (<>) but that's an easy fix 
-         * Added 'Full timetable' and 'Module timetable' buttons without function 
-         * I think that clicking either of these buttons should display a module specific timetable or a full timetable and not take a user to a new screen
-         */
+       
 
     }
 }
